828082f19ccedf83854820bdcbb1bb8728b335e9
PAOLO CAGLIERO s324194 <s281559@studenti.polito.it>
2025-03-21 11:37:15 +0100

    Generating a timetable for mechatronic. Instead of limiting the number con consecutive slots, I'm limiting the number of "correlations" in a day (WIP)

--- Data/DbAPI.py
+++ Data/DbAPI.py
@@ -14,19 +14,33 @@ class DbAPI:
             Get all the teachings in the DB
             Return: list of teachings in format [ID_INC, nStudenti, nStudentiFreq, collegio, titolo, CFU, oreLez, titolare]
         '''
+        # TODO: getting only the mechatronic teachings, instead of the whole DB. The final query should be: SELECT * FROM Insegnamento
         cur = self.db.cursor()
-        sql = "SELECT * FROM Insegnamento"
+        sql =   ("SELECT * FROM Insegnamento "
+                 "WHERE id_inc IN "
+                    "(SELECT id_inc FROM Insegnamento_in_Orientamento "
+                        "WHERE nomeCdl='MECHATRONIC ENGINEERING (INGEGNERIA MECCATRONICA)' "
+                        "AND orientamento='Control Technologies for Industry 4.0')")
         cur.execute(sql)
         teachings = cur.fetchall()
         return teachings
 
     def get_correlations_info(self):
         '''
-        Get all the correlations info in the DB
-        Return: list of correlations info in format [ID_INC_1, ID_INC_2, Correlazione, Correlazione_finale]
+            Get all the correlations info in the DB
+            Return: list of correlations info in format [ID_INC_1, ID_INC_2, Correlazione, Correlazione_finale]
         '''
+        # TODO: getting only the correlations for mechatronic teachings, instead of the whole DB. The final query should be: SELECT * FROM Info_correlazioni
         cur = self.db.cursor()
-        sql = "SELECT * FROM Info_correlazioni"
+        sql = ( "SELECT * FROM Info_correlazioni "
+                "WHERE id_inc_1 IN "
+                    "(SELECT id_inc FROM Insegnamento_in_Orientamento "
+                        "WHERE nomeCdl='MECHATRONIC ENGINEERING (INGEGNERIA MECCATRONICA)'"
+                        "AND orientamento='Control Technologies for Industry 4.0')"
+                "AND id_inc_2 IN "
+                    "(SELECT id_inc FROM Insegnamento_in_Orientamento "
+                    "WHERE nomeCdl='MECHATRONIC ENGINEERING (INGEGNERIA MECCATRONICA)'"
+                        "AND orientamento='Control Technologies for Industry 4.0')")
         cur.execute(sql)
         correlations = cur.fetchall()
         return correlations
--- main.py
+++ main.py
@@ -5,6 +5,15 @@ from Components.Slots import get_slots_per_week
 from Utils.Hooks.Teachings import Teachings
 from Utils.Parameters import Parameters
 
+'''
+    This function returns the correlation value between two teachings that are in the same day in different slots
+'''
+def get_corr_value():
+    if timetable_matrix[t, s] == 1:
+        return
+    else:
+        return 0
+
 if __name__ == '__main__':
     # Problem definition
     model = CpoModel(name="PoliTO_Timetable_Scheduling")
@@ -15,37 +24,45 @@ if __name__ == '__main__':
     teachings_class = Teachings()
     teachings = teachings_class.teachings
 
-    # TODO: limiting the number of teachings so that I stay under the 1000 vars included in the trial version of CPLEX
-    teachings = teachings[:25]
-
     # Number of slots per week
     slots = get_slots_per_week()
+    # Number of days per week
+    days = range(0, (6 if params.saturday_enabled else 5))
 
-    # Binary variables x[s,t] = 1 if the Teaching 't' is assigned to Slot 's'
-    x = {(t.id_teaching, s): model.binary_var(name=f"x_{t.id_teaching}_{s}") for t in teachings for s in slots}
+    # Weight of the Teachings in a day
+    day_load = [0] * (6 if params.saturday_enabled else 5)
+
+    # Binary variables timetable_matrix[t,s] = 1 if the Teaching 't' is assigned to Slot 's'
+    timetable_matrix = {(t.id_teaching, s): model.binary_var(name=f"x_{t.id_teaching}_{s}") for t in teachings for s in slots}
 
     # Constraint: each Teaching must have exactly cfu/2 Slots per week
     for t in teachings:
-        model.add_constraint(model.sum(x[t.id_teaching, s] for s in slots) == math.floor(t.cfu/2))
+        model.add_constraint(model.sum(timetable_matrix[t.id_teaching, s] for s in slots) == math.floor(t.cfu/2))
 
     # Constraint: a Teaching cannot overlap with the others, according to the correlations
     for s in slots:
         for t1 in teachings:
-            for t_id, corr in t1.correlations.items():
+            for t2_id, corr in t1.correlations.items():
                 # I need this if in order to not impose the same constraint twice (e.g. one from 267072 to 267158 and the other from 267158 to 267072)
-                if t1.id_teaching < t_id:
-                    # TODO: the following if is needed in order to run the program with a limited number of teachings (see line 15)
-                    if [t.id_teaching for t in teachings].__contains__(t_id):
-                        model.add_constraint(x[t1.id_teaching, s] + x[t_id, s] <= 1)
+                if t1.id_teaching < t2_id:
+                    model.add_constraint(timetable_matrix[t1.id_teaching, s] + timetable_matrix[t2_id, s] <= 1)
 
-    # Constraint: limiting the number of consecutive lecture slots (max 4 consecutive slots). I only consider the lectures that cannot overlap with each other, since this means that they are in the same Orientation
-    # TODO: needs to be tested with the complete DB
-    for s in range(len(slots) - (params.max_consecutive_slots + 1)):  # The -5 is used to avoid errors on the last slots
-        # The following if is needed to separate the slots between days. Each day has slot_per_day Slots, I don't need to apply a constraint between Monday 17:30-19:00 and Tuesday 8:30-10:00
-        if s % params.slot_per_day < params.slot_per_day - params.max_consecutive_slots:
-            for t in teachings:
-                teachings_in_same_orientation = [t.id_teaching] + list(t.correlations.keys())
-                model.add(model.sum(x[to, s + i] for i in range(0, params.max_consecutive_slots+1) for to in teachings_in_same_orientation if to in [teac.id_teaching for teac in teachings]) <= params.max_consecutive_slots)
+                # Constraint: limiting the number of "correlated lectures" in a day. For example, I impose that the sum of the correlations between lectures in a day should be < 400
+                # This way I don't limit the number of consecutive lecture slots
+                # TODO: needs to be tested
+                model.add_constraint(model.sum(corr * (timetable_matrix[t1.id_teaching, s] + timetable_matrix[t2_id, s + i]) for i in range(0, params.slot_per_day - (s % params.slot_per_day))) <= params.max_corr_in_day)
+
+    # Another idea:
+    # Constraint: limiting the number of "correlated lectures" in a day. For example, I impose that the sum of the correlations between lectures in a day should be < 400
+    # This way I don't limit the number of consecutive lecture slots
+    '''
+    for d in days:
+        dayly_load = 0
+        teachings_in_day = [t for t in teachings if (timetable_matrix[t.id_teaching, s] for s in slots if math.floor(s/params.slot_per_day) == d)]
+        for t in teachings_in_day:
+            print (t.id_teaching)
+        print("----")
+    '''
 
     # Solving the problem
     solution = model.solve(log_output=True)
@@ -54,6 +71,6 @@ if __name__ == '__main__':
     if solution:
         print("\nSolution found:")
         for t in teachings:
-            print(f"{t.id_teaching}: {[int(solution[x[t.id_teaching, s]]) for s in slots]}")
+                print(f"{t.id_teaching}: {[int(solution[timetable_matrix[t.id_teaching, s]]) for s in slots]}")
     else:
         print("No solution found.")
