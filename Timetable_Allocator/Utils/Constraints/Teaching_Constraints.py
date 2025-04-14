9af6adf3653dd21f6cd64b1d10906a9c9fe87f7a
Paolo Cagliero <s324194@studenti.polito.it>
2025-04-14 10:23:19 +0000

    Generated mechatronic timetable

--- Timetable_Allocator/Utils/Constraints/Teaching_Constraints.py
+++ Timetable_Allocator/Utils/Constraints/Teaching_Constraints.py
@@ -264,7 +264,7 @@ def add_teachings_constraints(model, timetable_matrix, teachings, slots, days):
     add_consecutive_slots_constraint(model, timetable_matrix, teachings, slots)
 
     # Constraint: the difference between the first and last lecture slot of the day should be minimized
-    add_first_last_lecture_of_day_limit(model, timetable_matrix, teachings, slots, days)
+    #add_first_last_lecture_of_day_limit(model, timetable_matrix, teachings, slots, days)
 
     # Constraint: the correlation between teachings in the first and last slot of the day should be <= params.max_corr_first_last_slot, in order to avoid that the majority of students starts at 8:30 and finishes at 19:00
     add_first_last_slot_correlation_limit(model, timetable_matrix, teachings, slots)
