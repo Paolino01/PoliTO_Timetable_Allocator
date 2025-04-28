a64ee2a606c3e79f4776b3fa071a745d374cb626
PAOLO CAGLIERO s324194 <s324194@studenti.polito.it>
2025-04-28 23:14:29 +0200

    Generating Timetable with all courses except Computer Engineering

--- Timetable_Allocator/Data/DbAPI.py
+++ Timetable_Allocator/Data/DbAPI.py
@@ -41,8 +41,8 @@ class DbAPI:
                     "double_slots_lab "
                  "FROM Insegnamento, Insegnamento_in_Orientamento "
                  "WHERE Insegnamento.ID_INC = Insegnamento_in_Orientamento.ID_INC "
-                    "AND nomeCdl = ?")
-        cur.execute(sql, ("INGEGNERIA INFORMATICA (COMPUTER ENGINEERING)", ))
+                    "AND nomeCdl NOT IN (?, ?)")
+        cur.execute(sql, ("INGEGNERIA INFORMATICA (COMPUTER ENGINEERING)", "INGEGNERIA INFORMATICA", ))
         teachings = cur.fetchall()
         return teachings
 
